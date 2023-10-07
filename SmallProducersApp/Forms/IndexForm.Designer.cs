3814c9b46cb37795c8ef707fc4133e487146db72
Fábio Goucha <fabiogoucha_1995@hotmail.com>
2023-10-07 15:09:36 +0100

    main menu visual change

--- SmallProducersApp/Forms/IndexForm.Designer.cs
+++ SmallProducersApp/Forms/IndexForm.Designer.cs
@@ -28,75 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IndexForm));
             btnClients = new Button();
             btnSuppiers = new Button();
             btnBuys = new Button();
             btnSells = new Button();
             btnAccounting = new Button();
             btnProducts = new Button();
+            button1 = new Button();
             SuspendLayout();
             // 
             // btnClients
             // 
-            btnClients.Location = new Point(54, 71);
+            btnClients.Image = (Image)resources.GetObject("btnClients.Image");
+            btnClients.ImageAlign = ContentAlignment.TopCenter;
+            btnClients.Location = new Point(12, 12);
             btnClients.Name = "btnClients";
-            btnClients.Size = new Size(102, 41);
+            btnClients.Size = new Size(110, 50);
             btnClients.TabIndex = 0;
             btnClients.Text = "Clientes";
+            btnClients.TextAlign = ContentAlignment.BottomCenter;
             btnClients.UseVisualStyleBackColor = true;
             btnClients.Click += btnClients_Click;
             // 
             // btnSuppiers
             // 
-            btnSuppiers.Location = new Point(162, 71);
+            btnSuppiers.Image = (Image)resources.GetObject("btnSuppiers.Image");
+            btnSuppiers.ImageAlign = ContentAlignment.TopCenter;
+            btnSuppiers.Location = new Point(128, 12);
             btnSuppiers.Name = "btnSuppiers";
-            btnSuppiers.Size = new Size(102, 41);
+            btnSuppiers.Size = new Size(110, 50);
             btnSuppiers.TabIndex = 1;
             btnSuppiers.Text = "Fornecedores";
+            btnSuppiers.TextAlign = ContentAlignment.BottomCenter;
             btnSuppiers.UseVisualStyleBackColor = true;
             // 
             // btnBuys
             // 
-            btnBuys.Location = new Point(270, 71);
+            btnBuys.Image = (Image)resources.GetObject("btnBuys.Image");
+            btnBuys.ImageAlign = ContentAlignment.TopCenter;
+            btnBuys.Location = new Point(306, 12);
             btnBuys.Name = "btnBuys";
-            btnBuys.Size = new Size(102, 41);
+            btnBuys.Size = new Size(110, 50);
             btnBuys.TabIndex = 2;
             btnBuys.Text = "Compras";
+            btnBuys.TextAlign = ContentAlignment.BottomCenter;
             btnBuys.UseVisualStyleBackColor = true;
             // 
             // btnSells
             // 
-            btnSells.Location = new Point(378, 71);
+            btnSells.Image = (Image)resources.GetObject("btnSells.Image");
+            btnSells.ImageAlign = ContentAlignment.TopCenter;
+            btnSells.Location = new Point(422, 12);
             btnSells.Name = "btnSells";
-            btnSells.Size = new Size(102, 41);
+            btnSells.Size = new Size(110, 50);
             btnSells.TabIndex = 3;
             btnSells.Text = "Vendas";
+            btnSells.TextAlign = ContentAlignment.BottomCenter;
             btnSells.UseVisualStyleBackColor = true;
             // 
             // btnAccounting
             // 
-            btnAccounting.Location = new Point(486, 71);
+            btnAccounting.Image = (Image)resources.GetObject("btnAccounting.Image");
+            btnAccounting.ImageAlign = ContentAlignment.TopCenter;
+            btnAccounting.Location = new Point(594, 12);
             btnAccounting.Name = "btnAccounting";
-            btnAccounting.Size = new Size(102, 41);
+            btnAccounting.Size = new Size(110, 50);
             btnAccounting.TabIndex = 4;
             btnAccounting.Text = "Estatísticas";
+            btnAccounting.TextAlign = ContentAlignment.BottomCenter;
             btnAccounting.UseVisualStyleBackColor = true;
             // 
             // btnProducts
             // 
-            btnProducts.Location = new Point(54, 135);
+            btnProducts.Image = (Image)resources.GetObject("btnProducts.Image");
+            btnProducts.ImageAlign = ContentAlignment.TopCenter;
+            btnProducts.Location = new Point(12, 97);
             btnProducts.Name = "btnProducts";
-            btnProducts.Size = new Size(102, 41);
+            btnProducts.Size = new Size(110, 50);
             btnProducts.TabIndex = 5;
             btnProducts.Text = "Produtos";
+            btnProducts.TextAlign = ContentAlignment.BottomCenter;
             btnProducts.UseVisualStyleBackColor = true;
             btnProducts.Click += btnProducts_Click;
+            // 
+            // button1
+            // 
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.TopCenter;
+            button1.Location = new Point(306, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 50);
+            button1.TabIndex = 6;
+            button1.Text = "Trabalhadores";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
             // 
             // IndexForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1234, 611);
+            Controls.Add(button1);
             Controls.Add(btnProducts);
             Controls.Add(btnAccounting);
             Controls.Add(btnSells);
@@ -116,5 +149,6 @@
         private Button btnSells;
         private Button btnAccounting;
         private Button btnProducts;
+        private Button button1;
     }
 }
